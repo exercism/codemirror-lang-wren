9deddbc21bf19ad1dda88d6c42e64b766607b2b6
Josh Goebel <me@joshgoebel.com>
2021-05-24 16:41:55 -0400

    cleanup

--- src/snippets.js
+++ src/snippets.js
@@ -1,41 +1,35 @@
 import {snippetCompletion as snip} from "@codemirror/autocomplete"
 
-/// A collection of JavaScript-related
-/// [snippets](#autocomplete.snippet).
+/// A collection of Wren-related snippets
 export const snippets = [
-  snip("function ${name}(${params}) {\n\t${}\n}", {
-    label: "function",
+  snip("Fn.new { |${args}| ${} }\n", {
+    label: "Fn.new",
     detail: "definition",
     type: "keyword"
   }),
-  snip("for (let ${index} = 0; ${index} < ${bound}; ${index}++) {\n\t${}\n}", {
+  snip("for (${name} in ${collection}) {\n\t${}\n}", {
     label: "for",
-    detail: "loop",
+    detail: "in loop",
     type: "keyword"
   }),
-  snip("for (let ${name} of ${collection}) {\n\t${}\n}", {
-    label: "for",
-    detail: "of loop",
-    type: "keyword"
-  }),
-  snip("try {\n\t${}\n} catch (${error}) {\n\t${}\n}", {
-    label: "try",
-    detail: "block",
-    type: "keyword"
-  }),
-  snip("class ${name} {\n\tconstructor(${params}) {\n\t\t${}\n\t}\n}", {
+  snip("class ${name} {\n\tconstruct new(${params}) {\n\t\t${}\n\t}\n}", {
     label: "class",
     detail: "definition",
     type: "keyword"
   }),
-  snip("import {${names}} from \"${module}\"\n${}", {
-    label: "import",
-    detail: "named",
+  snip("class ${name} is ${inherited} {\n\tconstruct new(${params}) {\n\t\tsuper()\n\t\t${}\n\t}\n}", {
+    label: "class",
+    detail: "definition (inherited)",
     type: "keyword"
   }),
-  snip("import ${name} from \"${module}\"\n${}", {
+  snip("import \"${name}\" for ${class}\n", {
     label: "import",
-    detail: "default",
+    detail: "for",
     type: "keyword"
-  })
+  }),
+  snip("import \"${name}\" for ${class} as ${rename}\n", {
+    label: "import",
+    detail: "as",
+    type: "keyword"
+  }),
 ]
--- src/wren.js
+++ src/wren.js
@@ -1,4 +1,3 @@
-// import {parser} from "lezer-javascript"
 import {parser} from "./lezer/parser.js"
 import {LezerLanguage, LanguageSupport,
         delimitedIndent, flatIndent, continuedIndent, indentNodeProp,
@@ -7,64 +6,44 @@ import {styleTags, tags as t} from "@codemirror/highlight"
 import {completeFromList, ifNotIn} from "@codemirror/autocomplete"
 import {snippets} from "./snippets"
 
-/// A language provider based on the [Lezer JavaScript
-/// parser](https://github.com/lezer-parser/javascript), extended with
-/// highlighting and indentation information.
 export const wrenLanguage = LezerLanguage.define({
   parser: parser.configure({
     props: [
       indentNodeProp.add({
         IfStatement: continuedIndent({except: /^\s*({|else\b)/}),
-        TryStatement: continuedIndent({except: /^\s*({|catch|finally)\b/}),
-        LabeledStatement: flatIndent,
-        SwitchBody: context => {
-          let after = context.textAfter, closed = /^\s*\}/.test(after), isCase = /^\s*(case|default)\b/.test(after)
-          return context.baseIndent + (closed ? 0 : isCase ? 1 : 2) * context.unit
-        },
         Block: delimitedIndent({closing: "}"}),
-        ArrowFunction: cx => cx.baseIndent + cx.unit,
-        "TemplateString BlockComment": () => -1,
+        BlockComment: () => -1,
         "Statement Property": continuedIndent({except: /^{/}),
-        JSXElement(context) {
-          let closed = /^\s*<\//.test(context.textAfter)
-          return context.lineIndent(context.state.doc.lineAt(context.node.from)) + (closed ? 0 : context.unit)
-        },
-        JSXEscape(context) {
-          let closed = /\s*\}/.test(context.textAfter)
-          return context.lineIndent(context.state.doc.lineAt(context.node.from)) + (closed ? 0 : context.unit)
-        },
-        "JSXOpenTag JSXSelfClosingTag"(context) {
-          return context.column(context.node.from) + context.unit
-        }
       }),
       foldNodeProp.add({
-        "Block ClassBody SwitchBody EnumBody ObjectExpression ArrayExpression": foldInside,
+        "Block ClassBody ObjectExpression ArrayExpression": foldInside,
         BlockComment(tree) { return {from: tree.from + 2, to: tree.to - 2} }
       }),
+      // as break class construct continue else false for foreign if import
+      // in is null return static super this true var while
       styleTags({
-        "get set async static construct foreign": t.modifier,
-        "for while do if else switch try catch finally return throw break continue default case": t.controlKeyword,
-        "in of await yield void typeof delete instanceof": t.operatorKeyword,
-        "export import let var const function class extends": t.definitionKeyword,
-        "with debugger from as new": t.keyword,
+        "static construct foreign": t.modifier,
+        "for while do if else return break continue": t.controlKeyword,
+        "in": t.operatorKeyword,
+        "import var class is": t.definitionKeyword,
+        "as": t.keyword,
+        // TODO: """ strings
         TemplateString: t.special(t.string),
-        Super: t.atom,
+        super: t.atom,
         BooleanLiteral: t.bool,
         this: t.self,
         null: t.null,
-        Star: t.modifier,
+        // TODO: what are we doing with this
         "Blah": t.modifier,
         "ClassName": t.className,
         "VariableName FieldName": t.variableName,
         "CallExpression/VariableName": t.function(t.variableName),
         VariableDefinition: t.definition(t.variableName),
-        Label: t.labelName,
         PropertyName: t.propertyName,
         "CallExpression/MemberExpression/PropertyName": t.function(t.propertyName),
         "FunctionDeclaration/VariableDefinition": t.function(t.definition(t.variableName)),
         "ClassDeclaration/ClassName": t.definition(t.className),
         "PropertyNameDefinition ClassMethodName": t.definition(t.propertyName),
-        UpdateOp: t.updateOperator,
         LineComment: t.lineComment,
         BlockComment: t.blockComment,
         Number: t.number,
@@ -75,27 +54,26 @@ export const wrenLanguage = LezerLanguage.define({
         CompareOp: t.compareOperator,
         RegExp: t.regexp,
         Equals: t.definitionOperator,
-        "Arrow : Spread": t.punctuation,
+        ".. ...": t.punctuation,
+        ":": t.punctuation,
         "( )": t.paren,
         "[ ]": t.squareBracket,
         "{ }": t.brace,
         ".": t.derefOperator,
-        ", ;": t.separator,
-
+        ",": t.separator
       })
     ]
   }),
   languageData: {
-    closeBrackets: {brackets: ["(", "[", "{", "'", '"', "`"]},
+    closeBrackets: {brackets: ["(", "[", "{", '"']},
     commentTokens: {line: "//", block: {open: "/*", close: "*/"}},
     indentOnInput: /^\s*(?:case |default:|\{|\}|<\/)$/,
-    wordChars: "$"
+    // wordChars: "$"
   }
 })
 
 
-/// Wren support. Includes [snippet](#lang-javascript.snippets)
-/// completion.
+/// Wren support. Includes snippets.
 export function wren(config = {}) {
   let lang = wrenLanguage
   return new LanguageSupport(lang, wrenLanguage.data.of({
