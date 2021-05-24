cb47bc838a4d1a915a7de1e2c04e6a4319235fca
Josh Goebel <me@joshgoebel.com>
2021-05-24 16:06:15 -0400

    cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,16 @@
 import {nodeResolve} from "@rollup/plugin-node-resolve"
+
 export default {
-  input: "./devtool/editor.js",
-  output: {
-    file: "./devtool/editor.bundle.js",
-    format: "iife"
-  },
-  plugins: [nodeResolve()]
+  input: "./src/index.js",
+  output: [{
+    format: "cjs",
+    file: "./dist/index.cjs"
+  }, {
+    format: "es",
+    file: "./dist/index.es.js"
+  }],
+  // external(id) { return !/^[\.\/]/.test(id) },
+  plugins: [
+    nodeResolve()
+  ]
 }
