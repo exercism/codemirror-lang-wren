e2842207fd39bb2f31019fbf61755b043d68dfaa
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2021-05-30 13:41:29 +0200

    Add typings

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+import { Completion } from "@codemirror/autocomplete"
+import { LanguageSupport, LezerLanguage } from "@codemirror/language"
+
+export const wrenLanguage: LezerLanguage
+export function wren(config?: {}): LanguageSupport
+export const snippets: Completion[]
+
